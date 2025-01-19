18cdabaf84b2aef444f6d0c61040f399bd946fa5
Anbeeld <anbeeld@gmail.com>
2025-01-19 18:47:05 +0100

    Fixed indentation and added new cross-library webpack minimizing

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,51 @@
 import path from 'path';
+import TerserPlugin from 'terser-webpack-plugin';
 
 const webConfig = {
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    extensionAlias: {
+      '.js': ['.js', '.ts'],
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-        extensionAlias: {
-            '.js': ['.js', '.ts'],
-        },
+  },
+  target: 'web',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve('./dist'),
+    filename: 'tilted.js',
+    library: {
+      name: 'Tilted',
+      type: 'var',
+      export: 'default',
     },
-    target: 'web',
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve('./dist'),
-        filename: 'tilted.js',
-        library: {
-            name: 'Tilted',
-            type: 'var',
-            export: 'default',
-        },
-    },
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            passes: 2,
+          },
+          mangle: {
+            properties: {
+              regex: /^_.*|^[^a-z]*$/
+            }
+          }
+        }
+      }),
+    ],
+  },
 };
 
 export default [webConfig];
